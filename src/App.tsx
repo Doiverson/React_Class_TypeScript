99696ac8888c636d4c6688c8d82b9cf709c56e06
Doiverson <doiverson.canada@gmail.com>
2022-07-09 19:33:04 -0700

    react ts done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+    color?: string;
+}
+
+interface AppState {
+    counter: number;
+}
+
+// Functional Component
+// const App = (props: AppProps): JSX.Element => {
+//     return <div>
+//         {props.color}
+//     </div>
+// };
+
+class App extends React.Component<AppProps, AppState> {
+
+    constructor(props: AppProps) {
+        super(props);
+        this.state = { counter: 0 }
+    }
+
+
+    // Either this or using constructor
+    // state = {
+    //     counter: 0
+    // }
+
+    onInc = (): void => {
+      this.setState({ counter: this.state.counter + 1 });
+    };
+
+    onDec = (): void => {
+      this.setState({ counter: this.state.counter -1 });
+    };
+
+    render () {
+    return (
+        <div className="App">
+          <button onClick={this.onInc}>Inc</button>
+          <button onClick={this.onDec}>Dec</button>
+          {this.state.counter}
+        </div>
+    );
+    }
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { App } from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from 'redux-thunk';
+import {reducers} from "./reducers";
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </React.StrictMode>
 );
 
